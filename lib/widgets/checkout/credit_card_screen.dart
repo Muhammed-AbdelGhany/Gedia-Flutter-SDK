77e871af24fc6b8417643498afeaa332ca0469a5
Muhammed-AbdelGhany <62287023+Muhammed-AbdelGhany@users.noreply.github.com>
2025-11-10 17:29:58 +0200

    Update credit_card_screen.dart

--- lib/widgets/checkout/credit_card_screen.dart
+++ lib/widgets/checkout/credit_card_screen.dart
@@ -850,7 +850,7 @@ class CreditCardScreenState extends State<CreditCardScreen> {
                                         borderRadius:
                                             BorderRadius.circular(8.0),
                                       ),
-                                      primary:
+                                      backgroundColor:
                                           widget.checkoutOptions.payButtonColor,
                                     ),
                                     child: Container(
@@ -911,7 +911,7 @@ class CreditCardScreenState extends State<CreditCardScreen> {
                                         borderRadius:
                                             BorderRadius.circular(8.0),
                                       ),
-                                      primary: widget
+                                      backgroundColor: widget
                                           .checkoutOptions.cancelButtonColor,
                                     ),
                                     child: Container(
